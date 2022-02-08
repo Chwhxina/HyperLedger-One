380c5b345a5752996105625d429d1345d35e4e24
hepeng <hepeng@3hea.com>
2022-02-08 17:33:21 +0800

    尝试解决 java 链码安装慢的问题

--- src/main/java/org/hepeng/CatContract.java
+++ src/main/java/org/hepeng/CatContract.java
@@ -84,7 +84,7 @@ public class CatContract implements ContractInterface {
 
         log.info(String.format("使用 name 查询 cat , name = %s" , name));
 
-        String query = String.format("{\"selector\":{\"name\":\"%s\"} , \"use_index\":[\"_design/indexNameColorDoc\", \"indexNameColor\"]}\"]}", name);
+        String query = String.format("{\"selector\":{\"name\":\"%s\"} , \"use_index\":[\"_design/indexNameColorDoc\", \"indexNameColor\"]\"}", name);
 
         log.info(String.format("query string = %s" , query));
         return queryCat(ctx.getStub() , query);
@@ -95,7 +95,7 @@ public class CatContract implements ContractInterface {
 
         log.info(String.format("使用 name & color 查询 cat , name = %s , color = %s" , name , color));
 
-        String query = String.format("{\"selector\":{\"name\":\"%s\" , \"color\":\"%s\"} , \"use_index\":[\"_design/indexNameColorDoc\", \"indexNameColor\"]}\"]}", name , color);
+        String query = String.format("{\"selector\":{\"name\":\"%s\" , \"color\":\"%s\"} , \"use_index\":[\"_design/indexNameColorDoc\", \"indexNameColor\"]\"}", name , color);
         return queryCat(ctx.getStub() , query);
     }
 
